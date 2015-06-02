fad394f4279a18a00fa12d02503f48c99e4afdf5
Yuen92 <bianconithomas@gmail.com>
2015-06-02 16:53:49 +0200

    DaoLocatorTest

--- Architecture/tests/com/architecture/server/DaoLocatorTest.java
+++ Architecture/tests/com/architecture/server/DaoLocatorTest.java
@@ -1,11 +1,18 @@
 package com.architecture.server;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DaoLocatorTest {
 	DaoLocator daoL;
+
+	public class DaoLocatorException {
+		private DaoLocatorException() {
+		}
+	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -18,5 +25,17 @@ public class DaoLocatorTest {
 
 	@Test
 	public void testGetInstance() {
+		daoL.getInstance(daoL.getClass());
+		assertNotNull(daoL);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testGetInstance_InstantiationException() {
+		daoL.getInstance(DaoLocatorException.class);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testGetInstance_IllegalAccessException() {
+		daoL.getInstance(DaoLocatorException.class.getClass());
 	}
 }
