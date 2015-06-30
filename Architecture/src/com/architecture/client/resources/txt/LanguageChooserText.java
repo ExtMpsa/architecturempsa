b4ea7406fd653dcf8daeb3c4e6038f7a3d421ca9
Yuen92 <bianconithomas@gmail.com>
2015-06-30 21:35:54 +0200

    Ajout de fichier non versionné

---
+++ Architecture/src/com/architecture/client/resources/txt/LanguageChooserText.java
@@ -0,0 +1,9 @@
+package com.architecture.client.resources.txt;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface LanguageChooserText extends Constants {
+	String fr();
+
+	String en();
+}
