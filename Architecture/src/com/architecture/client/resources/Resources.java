5bdf2bdd7ba7b64ecf95bcd44ab8d190135a22ed
Yuen92 <bianconithomas@gmail.com>
2015-07-07 12:01:43 +0200

    favicon Carstore

--- Architecture/src/com/architecture/client/resources/Resources.java
+++ Architecture/src/com/architecture/client/resources/Resources.java
@@ -5,7 +5,6 @@ import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.NotStrict;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.resources.client.TextResource;
 
 public interface Resources extends ClientBundle {
 	public static final Resources INSTANCE = GWT.create(Resources.class);
@@ -22,18 +21,18 @@ public interface Resources extends ClientBundle {
 
 	@Source("images/logo_peugeot.png")
 	ImageResource logoPeugeot();
-	
-	@Source("js/gtm-PJ7D96.js")
-	TextResource gtmJs();
-	
-	@Source("js/analytics.js")
-	TextResource analyticsJs();
-	
-	@Source("js/linkid.js")
-	TextResource linkidJs();
-	
-	@Source("js/ec.js")
-	TextResource ecJs();
+
+	// @Source("js/gtm-PJ7D96.js")
+	// TextResource gtmJs();
+	//
+	// @Source("js/analytics.js")
+	// TextResource analyticsJs();
+	//
+	// @Source("js/linkid.js")
+	// TextResource linkidJs();
+	//
+	// @Source("js/ec.js")
+	// TextResource ecJs();
 
 	public interface Style extends CssResource {
 		String logoPeugeotSprite();
