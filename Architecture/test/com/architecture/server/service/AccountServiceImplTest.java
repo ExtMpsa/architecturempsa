70ba9eb26fa2632c4427c96fd7cc30c9e4e69540
Yuen92 <bianconithomas@gmail.com>
2015-07-10 19:43:53 +0200

    AccountServiceTest

--- Architecture/test/com/architecture/server/service/AccountServiceImplTest.java
+++ Architecture/test/com/architecture/server/service/AccountServiceImplTest.java
@@ -9,33 +9,64 @@ import org.junit.Test;
 import org.slim3.datastore.Datastore;
 import org.slim3.tester.ServletTestCase;
 
+import com.architecture.server.meta.AccountMeta;
 import com.architecture.shared.model.Account;
 
 public class AccountServiceImplTest extends ServletTestCase {
 
 	private AccountServiceImpl service = new AccountServiceImpl();
+	private AccountMeta a = new AccountMeta();
 
 	@Test
 	public void test() throws Exception {
 		assertThat(service, is(notNullValue()));
 	}
+	
 
+	private final String mail = "mail@gmail.com";
+	private final String pwd = "password";
 	@Test
-	public void create() throws Exception {
-		service.create("mail@gmail.com", "password");
-		Account account = Datastore.query(Account.class).asSingle();
+	public void createWithEmptyDataStore() throws Exception {
+		service.create(mail, pwd);
+		Account account = Datastore.query(a).filter(a.mail.startsWith(mail)).asSingle();
 		assertThat(account, is(notNullValue()));
-		assertThat(account.getMail(), is("mail@gmail.com"));
+		assertThat(account.getMail(), is(mail));
+	}
+	
+	private final String mail2 = "mail2@gmail.com";
+	@Test
+	public void createWithNotEmptyDataStore() throws Exception {
+		service.create(mail, pwd);
+		Account account = Datastore.query(a).filter(a.mail.startsWith(mail)).asSingle();
+		assertThat(account, is(notNullValue()));
+		assertThat(account.getMail(), is(mail));
+		
+		service.create(mail2, pwd);
+		account = Datastore.query(a).filter(a.mail.startsWith(mail2)).asSingle();
+		assertThat(account, is(notNullValue()));
+		assertThat(account.getMail(), is(mail2));
 	}
 
 	@Test(expected = ConstraintViolationException.class)
-	public void createDouble() throws Exception {
-		service.create("mail@gmail.com", "password");
-		Account account = Datastore.query(Account.class).asSingle();
+	public void createWithMailUsed() throws Exception {
+		service.create(mail, pwd);
+		Account account = Datastore.query(a).filter(a.mail.startsWith(mail)).asSingle();
 		assertThat(account, is(notNullValue()));
-		assertThat(account.getMail(), is("mail@gmail.com"));
-
-		service.create("mail@gmail.com", "password");
+		assertThat(account.getMail(), is(mail));
+		
+		service.create(mail, pwd);
+	}
+	
+	private String emptyMail = "";
+	@Test(expected = ConstraintViolationException.class)
+	public void createWithLengthConstraintMailViolation() throws Exception {
+		service.create(emptyMail, pwd);
+	}
+	
+	private String wrongMail = "mail";
+	@Test(expected = ConstraintViolationException.class)
+	public void createWithFormatConstraintMailViolation() throws Exception {
+		service.create(wrongMail, pwd);
 	}
 
 	@Test
