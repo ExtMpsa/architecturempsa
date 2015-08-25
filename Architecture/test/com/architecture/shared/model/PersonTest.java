3bd004a411c64e490a1b9144516c46d3f7df0736
Yuen92 <bianconithomas@gmail.com>
2015-08-25 09:46:15 +0200

    Tests

--- Architecture/test/com/architecture/shared/model/PersonTest.java
+++ Architecture/test/com/architecture/shared/model/PersonTest.java
@@ -1,16 +1,27 @@
 package com.architecture.shared.model;
 
-import org.slim3.tester.AppEngineTestCase;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.slim3.tester.AppEngineTestCase;
 
 public class PersonTest extends AppEngineTestCase {
 
-    private Person model = new Person();
+	private Person model = new Person();
 
-    @Test
-    public void test() throws Exception {
-        assertThat(model, is(notNullValue()));
-    }
+	@Test
+	public void test() throws Exception {
+		assertThat(model, is(notNullValue()));
+	}
+
+	@Test
+	public void equalTrue() throws Exception {
+		assertThat(model.equals(model), is(true));
+	}
+
+	@Test
+	public void equalFalse() throws Exception {
+		assertThat(model.equals("true"), is(false));
+	}
 }
