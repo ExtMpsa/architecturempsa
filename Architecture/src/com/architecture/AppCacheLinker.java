590f74c79b7c58af162b102e05e25704b98ed050
Yuen92 <bianconithomas@gmail.com>
2015-08-31 14:09:22 +0200

    App Cache init

--- Architecture/src/com/architecture/AppCacheLinker.java
+++ Architecture/src/com/architecture/AppCacheLinker.java
@@ -1,100 +1,50 @@
 package com.architecture;
 
-import java.util.HashSet;
-import java.util.SortedSet;
-
 import com.google.gwt.core.ext.LinkerContext;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.linker.AbstractLinker;
-import com.google.gwt.core.ext.linker.Artifact;
 import com.google.gwt.core.ext.linker.ArtifactSet;
 import com.google.gwt.core.ext.linker.EmittedArtifact;
 import com.google.gwt.core.ext.linker.LinkerOrder;
 import com.google.gwt.core.ext.linker.LinkerOrder.Order;
-import com.google.gwt.core.ext.linker.Shardable;
-import com.google.gwt.core.ext.linker.impl.SelectionInformation;
 
-@Shardable
 @LinkerOrder(Order.POST)
 public class AppCacheLinker extends AbstractLinker {
-	private static String MANIFEST;
-
 	@Override
 	public String getDescription() {
-		return "AppCacheLinker";
+		return "AppCache Linker";
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public ArtifactSet link(TreeLogger logger, LinkerContext context, ArtifactSet artifacts, boolean onePermutation) throws UnableToCompleteException {
-		MANIFEST = context.getModuleName() + "-artifacts.lst";
+	public ArtifactSet link(TreeLogger logger, LinkerContext context, ArtifactSet artifacts) throws UnableToCompleteException {
+		ArtifactSet artifactset = new ArtifactSet(artifacts);
 
-		ArtifactSet toReturn = new ArtifactSet(artifacts);
+		StringBuilder builder = new StringBuilder("CACHE MANIFEST\n");
+		builder.append("# Cache Version 10\n");
+		builder.append("CACHE:\n");
 
-		logger.log(TreeLogger.INFO, "SelectionInformation:");
-		SortedSet<SelectionInformation> selectionInformationSet = toReturn.find(SelectionInformation.class);
-		SelectionInformation selectionInformation = selectionInformationSet.isEmpty() ? null : selectionInformationSet.first();
-
-		HashSet<String> classes = new HashSet<>();
-		for (Artifact<?> a : artifacts) {
-			classes.add(a.getClass().getName());
-		}
-		logger.log(TreeLogger.INFO, classes.toString());
-
-		if (onePermutation) {
-			return toReturn;
-		}
-
-		if (toReturn.find(SelectionInformation.class).isEmpty()) {
-			logger.log(TreeLogger.INFO, "DevMode warning: Clobbering " + MANIFEST + " to allow debugging. Recompile before deploying your app!" + artifacts);
-			// artifacts = null;
-			return toReturn;
-		}
-
-		// Create the general cache-manifest resource for the landing page:
-		if (selectionInformation != null) {
-			toReturn.add(emitLandingPageCacheManifest("../" + context.getModuleName() + "-artifacts.lst", context, logger, artifacts));
-		}
-		return toReturn;
-	}
-
-	/**
-	 * Creates the cache-manifest resource specific for the landing page.
-	 * 
-	 * @param context
-	 *            the linker environment
-	 * @param logger
-	 *            the tree logger to record to
-	 * @param artifacts
-	 *            {@code null} to generate an empty cache manifest
-	 */
-	@SuppressWarnings("rawtypes")
-	private Artifact<?> emitLandingPageCacheManifest(String fileName, LinkerContext context, TreeLogger logger, ArtifactSet artifacts) throws UnableToCompleteException {
-		StringBuilder publicSourcesSb = new StringBuilder();
-
-		if (artifacts != null) {
-			// Iterate over all emitted artifacts, and collect all cacheable
-			// artifacts
-			for (Artifact artifact : artifacts) {
-				if (artifact instanceof EmittedArtifact) {
-					EmittedArtifact ea = (EmittedArtifact) artifact;
-					String pathName = ea.getPartialPath();
-					if (pathName.endsWith("symbolMap") || pathName.endsWith(".xml.gz") || pathName.endsWith("rpc.log") || pathName.endsWith("gwt.rpc")
-							|| pathName.endsWith("manifest.txt") || pathName.startsWith("rpcPolicyManifest") || pathName.startsWith("soycReport") || pathName.endsWith(".cssmap")) {
-						continue;// skip these resources
-					} else {
-						publicSourcesSb.append(context.getModuleName()).append("/").append(pathName.replace("\\", "/")).append("\n");
-					}
-				}
+		builder.append("/Architecture.html\n");
+		for (EmittedArtifact emitted : artifacts.find(EmittedArtifact.class)) {
+			if (emitted.isPrivate()) {
+				continue;
 			}
+			if (emitted.getPartialPath().endsWith(".symbolMap")) {
+				continue;
+			}
+			if (emitted.getPartialPath().endsWith(".txt")) {
+				continue;
+			}
+			// builder.append("/architecture/" + emitted.getPartialPath()).append("\n");
+			builder.append(emitted.getPartialPath()).append("\n");
 		}
-
-		// build cache list
-		StringBuilder sb = new StringBuilder();
-		sb.append(publicSourcesSb);
-		logger.log(TreeLogger.DEBUG, "Be sure your landing page's <html> tag declares a manifest: <html manifest=" + MANIFEST + "\">");
-
-		// Create the manifest as a new artifact and return it:
-		return emitString(logger, sb.toString(), fileName);
+		builder.append("/architecture/hosted.html\n");
+		builder.append("/architecture/architecture.nocache.js\n");
+		builder.append("NETWORK:\n");
+		builder.append("*\n");
+		EmittedArtifact manifest = emitString(logger, builder.toString(), "appcache.manifest");
+		artifactset.add(manifest);
+		return artifactset;
 	}
 }
