24e7a9254544d5c080f03b854d9a295f77f92608
Yuen92 <bianconithomas@gmail.com>
2015-09-16 22:08:15 +0200

    Catch exception first load manifest

--- Architecture/src/com/architecture/server/appcache/ManifestServlet.java
+++ Architecture/src/com/architecture/server/appcache/ManifestServlet.java
@@ -1,6 +1,8 @@
 package com.architecture.server.appcache;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,7 @@ import com.googlecode.mgwt.linker.server.propertyprovider.UserAgentPropertyProvi
 
 public class ManifestServlet extends Html5ManifestServletBase {
 	private static final long serialVersionUID = 3027999903018474841L;
+	private final static Logger LOGGER = Logger.getLogger(ManifestServlet.class.getName());
 
 	public ManifestServlet() {
 		addPropertyProvider(new LocalePropertyProvider());
@@ -19,9 +22,18 @@ public class ManifestServlet extends Html5ManifestServletBase {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
-		resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
-		resp.setHeader("Pragma", "no-cache"); // HTTP 1.0
-		resp.setDateHeader("Expires", 0);
+		try {
+			super.doGet(req, resp);
+			resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+			resp.setHeader("Pragma", "no-cache"); // HTTP 1.0
+			resp.setDateHeader("Expires", 0);
+			LOGGER.log(Level.INFO, "req = " + req.getRequestURL());
+		} catch (Exception e) {
+			LOGGER.log(Level.INFO, e.getMessage());
+			resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+			resp.setHeader("Pragma", "no-cache"); // HTTP 1.0
+			resp.setDateHeader("Expires", 0);
+		}
+
 	}
 }
