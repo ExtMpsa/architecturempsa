c237e4e06d73e1f072740c992ce8ac751ccb546a
Yuen92 <bianconithomas@gmail.com>
2015-09-16 17:32:49 +0200

    Fix WebPagetest

--- Architecture/src/com/architecture/server/appcache/ManifestServlet.java
+++ Architecture/src/com/architecture/server/appcache/ManifestServlet.java
@@ -1,5 +1,11 @@
 package com.architecture.server.appcache;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.googlecode.mgwt.linker.server.Html5ManifestServletBase;
 import com.googlecode.mgwt.linker.server.propertyprovider.UserAgentPropertyProvider;
 
@@ -9,5 +15,14 @@ public class ManifestServlet extends Html5ManifestServletBase {
 	public ManifestServlet() {
 		addPropertyProvider(new LocalePropertyProvider());
 		addPropertyProvider(new UserAgentPropertyProvider());
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+		resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+		resp.setHeader("Pragma", "no-cache"); // HTTP 1.0
+		resp.setDateHeader("Expires", 0);
+
 	}
 }
