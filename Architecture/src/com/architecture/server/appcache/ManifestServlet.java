ad4c128e426f54e11668bf395f75a472cda2e452
Yuen92 <bianconithomas@gmail.com>
2015-09-16 10:31:19 +0200

    Full utilisation de mgwt pour gérer le cache (manifest) TODO : gérer l'internationalisation

---
+++ Architecture/src/com/architecture/server/appcache/ManifestServlet.java
@@ -0,0 +1,12 @@
+package com.architecture.server.appcache;
+
+import com.googlecode.mgwt.linker.server.Html5ManifestServletBase;
+import com.googlecode.mgwt.linker.server.propertyprovider.UserAgentPropertyProvider;
+
+public class ManifestServlet extends Html5ManifestServletBase {
+	private static final long serialVersionUID = 3027999903018474841L;
+
+	public ManifestServlet() {
+		addPropertyProvider(new UserAgentPropertyProvider());
+	}
+}
