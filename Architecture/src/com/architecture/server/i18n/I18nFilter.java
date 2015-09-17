735b06cf1eddff77d086ed28cc253eeb087f429c
Yuen92 <bianconithomas@gmail.com>
2015-09-18 00:38:11 +0200

    Fix Webpagetest

--- Architecture/src/com/architecture/server/i18n/I18nFilter.java
+++ Architecture/src/com/architecture/server/i18n/I18nFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class I18nFilter implements Filter {
 	private final static Logger LOGGER = Logger.getLogger(I18nFilter.class.getName());
@@ -26,6 +27,11 @@ public class I18nFilter implements Filter {
 		String requestURI = httpRequest.getRequestURI();
 		LOGGER.log(Level.INFO, "request = " + httpRequest.getRequestURL());
 		LOGGER.log(Level.INFO, "request uri = " + httpRequest.getRequestURI());
+
+		final HttpServletResponse httpResponse = (HttpServletResponse) resp;
+		httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+		httpResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0
+		httpResponse.setDateHeader("Expires", 0);
 
 		if (requestURI.startsWith("/fr/")) {
 			String toReplace = "/fr/";
